88427803e5f10d08c74346768ad1be82473deeb2
AntonyPry <74418044+AntonyPry@users.noreply.github.com>
2023-02-15 12:22:02 +0300

    Первая лаба

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
-//http://localhost/test_1/index.php
-echo ("Hello, World!")
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src='logo.png'>
+        <p>Прытов Антон Сергеевич 221-321</p>
+    </header>
+    <?php echo '<p>Hello, World!</p>'; 
+    //     http://localhost/test_1/PHP_Polytech/index.php?>
 
+<footer>
+    <p>2.1.Домашняя работа: Hello, World!</p>
+</footer>
+
+</body>
+</html>
